60f0c3f3d7545b04d820d62c9f88dd9b13867e92
Advaita <148793147+01adv@users.noreply.github.com>
2024-04-02 13:51:21 +0530

    navbar with aceternity ui

---
+++ music-mg/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+   <main className=" h-[120vh] bg-black/80 antialiased ">
+    <h1 className="text-center text-3xl">Hello Frands</h1>
+   </main>
+  );
+}
