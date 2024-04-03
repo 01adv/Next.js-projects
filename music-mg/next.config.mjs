f4bced2adcb5f50045c9e42b6d5b5245368f10ca
Advaita <148793147+01adv@users.noreply.github.com>
2024-04-04 01:21:37 +0530

    done with meteor shower in contact page + interface type in course page

--- music-mg/next.config.mjs
+++ music-mg/next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        domains: ['images.unsplash.com']
+    }
+};
 
 export default nextConfig;
+
+// new pattern
+// images: {
+//   remotePatterns: [
+//     {
+//       protocol: 'https',
+//       hostname: 'assets.example.com',
+//       port: '',
+//       pathname: '/account123/**',
+//     },
+//   ],
+// },
--- music-mg/src/app/page.tsx
+++ music-mg/src/app/page.tsx
@@ -1,7 +1,21 @@
+import FeaturedCourses from "./component/FeaturedCourses";
+import Footer from "./component/Footer";
+import HeroSection from "./component/HeroSection";
+import Instructors from "./component/Instructors";
+import TestimonialCards from "./component/TestimonialCards";
+import UpcomingWebinar from "./component/UpcomingWebinar";
+import WhyChooseUs from "./component/WhyChooseUs";
+
 export default function Home() {
   return (
-   <main className=" h-[120vh] bg-black/80 antialiased ">
-    <h1 className="text-center text-3xl">Hello Frands</h1>
-   </main>
+    <main className=" h-[120vh] bg-black/80 antialiased ">
+      <HeroSection />
+      <FeaturedCourses/>
+      <WhyChooseUs/>
+      <TestimonialCards/>
+      <UpcomingWebinar/>
+      <Instructors/>
+      <Footer/>
+    </main>
   );
 }
