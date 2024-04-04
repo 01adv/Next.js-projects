43cbc631bb0bea3cafc5ac7519041d6d74d66dd3
Advaita <148793147+01adv@users.noreply.github.com>
2024-04-04 10:35:56 +0530

    new remote pattern replaced deprecated domain config in next mjs

--- music-mg/next.config.mjs
+++ music-mg/next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains: ['images.unsplash.com']
-    }
+    // images:{
+    //     domains: ['images.unsplash.com']
+    // },
+    images: {
+          remotePatterns: [
+            {
+              protocol: 'https',
+              hostname: 'images.unsplash.com',
+            },
+          ],
+        },
 };
 
 export default nextConfig;
