4157cda7ac6de932b948ad440734ee4c8852dc90
Advaita <148793147+01adv@users.noreply.github.com>
2024-04-05 20:09:27 +0530

    committing all files

---
+++ nextJsAuth/nextauth/src/helpers/mailer.ts
@@ -0,0 +1,43 @@
+import User from "@/models/userModel";
+import nodemailer from "nodemailer";
+import bcrypt from "bcryptjs";
+
+export const sendEmail = async ({ email, emailType, userId }: any) => {
+  try {
+    const hashedToken = await bcrypt.hash(userId.toString(), 10);
+    if (emailType === "VERIFY") {
+      await User.findByIdAndUpdate(userId, {
+        verifyToken: hashedToken,
+        verifyTokenExpiry: Date.now() + 3600000,
+      });
+    } else if (emailType === "RESET") {
+      await User.findByIdAndUpdate(userId, {
+        forgotPasswordToken: hashedToken,
+        forgotPasswordTokenExpiry: Date.now() + 3600000,
+      });
+    }
+
+    //create id on mailtrap and use credentials provided by them
+    const transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: true,
+      auth: {
+        user: "maddison53@ethereal.email",
+        pass: "jn7jnAPss4f63QBp6D",
+      },
+    });
+    const mailOptions = {
+      from: '"Advaita 👻" <07advaita@gmail.com>',
+      to: email,
+      subject:
+        emailType === "VERIFY" ? "Verify your email" : "Reset Your Password",
+      html: "<b>Hello world?</b>", // html body create the message body for both verify and reset email 
+    };
+
+    const mailResponse = await transporter.sendMail(mailOptions);
+    return mailResponse;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
