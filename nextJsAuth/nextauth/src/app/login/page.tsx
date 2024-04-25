fe6d83d353a77bc2048463a2f5a94c3b70dff417
Advaita <148793147+01adv@users.noreply.github.com>
2024-04-25 12:39:13 +0530

    upto login

--- nextJsAuth/nextauth/src/app/login/page.tsx
+++ nextJsAuth/nextauth/src/app/login/page.tsx
@@ -1,9 +1,71 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from "react";
 
-const Login = () => {
-  return (
-    <div>Login</div>
+import axios from "axios";
+import {toast} from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+export default function Login() {
+
+  const router = useRouter();
+
+  const [user, setUser] = useState({
+    email:"",
+    password: ""
+  });
+
+  const [buttonDisabled, setButtonDisabled] = useState(false)
+
+  const [loading, setLoading] = useState(false)
+
+  const onLogin = async () => {
+    try {
+      setLoading(true)
+      
+      const response = await axios.post(
+        "/api/users/login",
+        user
+      );
+      console.log(response.data);
+      toast.success("Login Successful");
+      router.push("/profile");
+      // setLoading(false)
+      // setButtonDisabled(false)
+
+    } catch (error: any) {
+      console.log("Login Failed", error);
+      toast.error(error.response.data.message);
+      // setLoading(false)
+      // setButtonDisabled(false)
+    }
+  }
+
+  useEffect(() =>{
+    if(user.email.length > 0 && user.password.length > 0){
+      setButtonDisabled(false)
+    }
+    else(
+      setButtonDisabled(true)
+    )
+  }, [user])
+
+  return(
+
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>{loading? "Loading..." : "Sign Up"}</h1>
+      <hr />
+
+      <label htmlFor="email"></label>
+      <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-indigo-300 text-black" id="email" type="email" name="email" placeholder="Email" value = {user.email} onChange={(e) => setUser({...user, email: e.target.value})} />
+      <hr />
+
+      <label htmlFor="password"></label>
+      <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-indigo-300 text-black" id="password" type="password" name="password" placeholder="Password" value = {user.password} onChange={(e) => setUser({...user, password: e.target.value})} />
+      <hr />
+
+      <button onClick={onLogin} className=" p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">{buttonDisabled? "No Login" : "Login  "}</button>
+      <Link href={"/login"}>Visit Login Page</Link>
+    </div>
   )
 }
-
-export default Login
